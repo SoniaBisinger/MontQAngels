322f8f2767ce8ffa4f6e7928b1195398f6e6c93a
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-05-30 16:35:32 +0200

    Modifying toilet paper table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_123728) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_143137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,14 +53,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_123728) do
 
   create_table "toilet_papers", force: :cascade do |t|
     t.string "color"
-    t.string "thikness"
+    t.string "thickness"
     t.string "scent"
-    t.integer "lenght"
     t.integer "price"
     t.boolean "used"
     t.boolean "premium"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "photo_url"
   end
 
   create_table "users", force: :cascade do |t|
