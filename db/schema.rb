e0d1636bd08596632fbe750a4e61dce3c5fde591
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-05-31 14:34:28 +0200

    adding total price to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_094453) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_123336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_094453) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "toilet_paper_id"
+    t.integer "total_price"
     t.index ["toilet_paper_id"], name: "index_bookings_on_toilet_paper_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
