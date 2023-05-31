a03a635238974330145d225cc7801fcc90da44db
simon despres <simondprs62790@hotmail.fr>
2023-05-31 14:46:04 +0200

    bonne indentation toilet_papers_controller

--- app/controllers/toilet_papers_controller.rb
+++ app/controllers/toilet_papers_controller.rb
@@ -1,6 +1,6 @@
 class ToiletPapersController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i [:index, :show],
-  before_action :set_toilet_paper, only: %i[:destroy, :update]
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_toilet_paper, only: %i[destroy update]
 
   def index
     @toilet_papers = ToiletPaper.all
@@ -13,35 +13,35 @@ class ToiletPapersController < ApplicationController
   def new
     if @user == current_user
       @toilet_paper = ToiletPaper.new
-    else 
+    else
       puts "you have to log-in to create a new product"
-    end 
+    end
   end
 
   def create
     @toilet_paper = ToiletPaper.new(toilet_paper_params)
     @toilet_paper.user = current_user
-      if @toilet_paper.save
-          redirect_to root_path
-      else
-          render :new, status: :unprocessable_entity
-      end
+    if @toilet_paper.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
-    if user == current_user
-      @toilet_paper = ToiletPaper.find(params[:id])
-    end 
-  end 
+    return unless user == current_user
+
+    @toilet_paper = ToiletPaper.find(params[:id])
+  end
 
   def update
     @toilet_paper.update(toilet_paper_params)
-  end 
+  end
 
   def destroy
     @toilet_paper.destroy
-    redirect_to root_pathgit 
-  end 
+    redirect_to root_pathgit
+  end
 
   private
 
@@ -49,8 +49,7 @@ class ToiletPapersController < ApplicationController
     @toilet_paper = ToiletPaper.find(params[:id])
   end
 
-
   def toilet_paper_params
-      params.require(:toilet_paper).permit(:title, :photo_url, :color, :thikness, :scent, :price, :used, :premium)
+    params.require(:toilet_paper).permit(:title, :photo_url, :color, :thikness, :scent, :price, :used, :premium)
   end
 end
