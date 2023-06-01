cf2163c6d49de25bd0ad7bf7287944bdc5f44cb1
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-06-01 13:22:57 +0200

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,19 @@ puts "Creating 5 new users..."
     )
 end
 puts "Finished users"
-
+thicknessese = ["single-ply", "2-ply", "3-ply", "4-ply"]
+colors = ["pink", "yellow", "black"]
 User.all.each do |user|
     puts "Creating 1 to 5 new toilet papers for user #{user.id}"
     rand(1..5).times.each do
         user.toilet_papers.create!({
-            title: Faker::TvShows::SouthPark.episode_name,
+            color: colors[rand(1)],
+            thickness: thicknessese[rand(1)],
+            scent: Faker::Dessert.flavor,
             price: Faker::Number.number(digits: 2),
+            title: Faker::TvShows::SouthPark.episode_name,
+            description: Faker::Coffee.notes,
+            photo_url: "image1.jpg",
             user:
         })
     end
